94df88f66d64a7768fbe132c11065bb24bc722b7
callum stringer <callumstringer10@gmail.com>
2021-04-20 23:40:45 +0200

    added tailwind and craco config

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Issue Tracker
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="bg-gray-100">
+                <h1>test</h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
